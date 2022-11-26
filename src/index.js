d72ba1bf5bc43a76547d92e9ce3636533455cd21
Radon4ik <Loboda.Radimir@gmail.com>
2022-11-27 01:54:46 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
 "use strict";
 
 import "./index.scss";
-
-let offSet = 0;
-
-const sliderLine = document.querySelector(".slider__line");
-let sliderContainer = document.querySelector(".slider__container");
-
-document.querySelector(".slider__btn-right").addEventListener("click", () => {
-  offSet += 1120;
-
-  sliderLine.style.left = -offSet + "px";
-  let div = document.createElement("div");
-  console.log(sliderLine.style.left);
-});
-
-document.querySelector(".slider__btn-left").addEventListener("click", () => {
-  offSet -= 1120;
-
-  sliderLine.style.left = -offSet + "px";
-  console.log(sliderLine.style.left);
-});

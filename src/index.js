50a3f9be91e756f6715f6c210bbbfa70680cc997
Радимир <loboda.radimir@gmail.com>
2023-01-20 16:07:55 +0200

    added js

--- src/index.js
+++ src/index.js
@@ -33,3 +33,37 @@ export const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+const burgerButton = document.querySelector(".burger-button");
+const navMenu = document.querySelector(".nav");
+
+burgerButton.addEventListener("click", () => {
+  navMenu.classList.toggle("menu-open");
+  burgerButton.classList.toggle("burger-button-open");
+  console.log(burgerButton);
+});
+
+const reviews = document.querySelector(".reviews");
+
+const blogs = document.querySelector(".blogs");
+
+function toggle(element, selector) {
+  element.addEventListener("click", (event) => {
+    if (
+      event.target.className === selector[0] ||
+      event.target.className === selector[1]
+    ) {
+      event.target.classList.toggle(selector[1].split(" ")[1]);
+    }
+    for (let i = 0; i < 2; i++) {
+      swiper[i].update();
+    }
+  });
+}
+
+toggle(reviews, [
+  "swiper-slide__description",
+  "swiper-slide__description swiper-slide__description-open",
+]);
+
+toggle(blogs, ["blogs__text", "blogs__text blog__text-open"]);
